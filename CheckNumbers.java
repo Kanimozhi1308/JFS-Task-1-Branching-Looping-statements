c88874b327ccee6290de0349f72d2c56cc7f8a98
Kanimozhi1308 <kanisk1309@gmail.com>
2025-07-27 17:25:24 +0530

    Create CheckNumbers.java

---
+++ CheckNumbers.java
@@ -0,0 +1,36 @@
+package conditional;
+
+import java.util.Scanner;
+
+public class CheckNumbers {
+	
+	public static void main (String args[]) {
+	    
+		Scanner num = new Scanner(System.in);
+		System.out.print("Enter the Number = ");
+		int n = num.nextInt();
+		
+		if(n > 0) {
+			System.out.println( "The Given Number is Positive");
+		}else if (n < 0) {
+			System.out.println( "The Given Number is Negative");
+		}else {
+			System.out.println( "The Given Number is Neither Positive nor Negative");
+		}
+			
+		}
+  
+  /*int n = 123;
+	
+	if(n > 0) {
+		
+		System.out.println( "The Given Number is Positive");
+	}else if (n < 0) {
+		System.out.println( "The Given Number is Negative");
+	}else {
+		System.out.println( "The Given Number is Neither Positive nor Negative");
+	}
+		
+	} */	
+		
+}
