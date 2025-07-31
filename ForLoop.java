6066eec06622f282aaf30b6e17cc7700d74d5813
Kanimozhi K <kanisk1309@gmail.com>
2025-07-31 11:00:50 +0530

    ForLoop.java

--- ForLoop.java
+++ ForLoop.java
@@ -4,9 +4,9 @@ public class ForLoop {
 
 		public static void main (String args[]) {
 			
-			int n = 50; int a = 10;
+			int n = 50; 
 			
-			for(a = 10; a<=n; a++)
+			for(int a = 10; a<=n; a++)
 			{
 				System.out.print(" "+ a);
 				
